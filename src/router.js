48263183dbeabd5abac546001a04aa58c7146815
AaronXue <xch1523480@163.com>
2020-02-12 13:57:56 +0800

    commit

--- src/router.js
+++ src/router.js
@@ -1,8 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-const page1 = () => import('./views/page1-1.vue')
-const page2 = () => import('./views/page1-2.vue')
+const page1 = () => import('./pages/page1.vue')
 
 Router.prototype.push = function (location, onComplete, onAbort) {
     this.isBack = false;                //判断当前路由是否是返回，用于App.vue中给路由跳转添加动画
@@ -18,20 +17,12 @@ export default new Router({
         {
             path: '/',
             name: 'index',
-            redirect: '/page1.html'
+            redirect: '/page1'
         },
         {
-            path: '/page1.html',
+            path: '/page1',
             name: 'page1',
             component: page1,
-            meta: {             //用于main.js中页面动态添加title
-                title: '神州邦邦'
-            }
-        },
-        {
-            path: '/page2.html',
-            name: 'page2',
-            component: page2,
             meta: {             //用于main.js中页面动态添加title
                 title: '神州邦邦'
             }
--- vue.config.js
+++ vue.config.js
@@ -1,42 +1,42 @@
-let glob = require('glob')
+// let glob = require('glob')
 
-//配置pages多页面获取当前文件夹下的html和js
-function getEntry(globPath) {
-    let entries = {}, tmp;
+// //配置pages多页面获取当前文件夹下的html和js
+// function getEntry(globPath) {
+//     let entries = {}, tmp;
 
-    // 读取src/pages/**/底下所有的html文件
-    glob.sync('./src/pages' + globPath + '/**/*.html').forEach(function (entry) {
-        tmp = entry.split('/').splice(-3);
-        entries[tmp[1]] = {
-            entry: entry.replace('html', 'js'),
-            template: entry,
-            filename: tmp[1] + '.html',   //  以文件夹名称.html作为访问地址
-            chunks: ["chunk-vendors", "chunk-common", tmp[1]]
-        };
-    })
-    return entries;
-}
-let htmls;
-process.argv.splice(0, 5)           //取掉前两个参数
-if (process.argv.length) {
-    for (let i = 0; i < process.argv.length; i++) {
-        const element = process.argv[i];
-        htmls = {
-            ...htmls,
-            ...getEntry('/' + element)
-        }
-    }
-} else {
-    // 若无传入页面参数，则全块打包
-    htmls = getEntry('')
-}
+//     // 读取src/pages/**/底下所有的html文件
+//     glob.sync('./src/pages' + globPath + '/**/*.html').forEach(function (entry) {
+//         tmp = entry.split('/').splice(-3);
+//         entries[tmp[1]] = {
+//             entry: entry.replace('html', 'js'),
+//             template: entry,
+//             filename: tmp[1] + '.html',   //  以文件夹名称.html作为访问地址
+//             chunks: ["chunk-vendors", "chunk-common", tmp[1]]
+//         };
+//     })
+//     return entries;
+// }
+// let htmls;
+// process.argv.splice(0, 5)           //取掉前两个参数
+// if (process.argv.length) {
+//     for (let i = 0; i < process.argv.length; i++) {
+//         const element = process.argv[i];
+//         htmls = {
+//             ...htmls,
+//             ...getEntry('/' + element)
+//         }
+//     }
+// } else {
+//     // 若无传入页面参数，则全块打包
+//     htmls = getEntry('')
+// }
 
 module.exports = {
     productionSourceMap: false,     //打包去掉sourceMap
     publicPath: process.env.NODE_ENV === 'production'
         ? './'
         : '/',
-    pages: htmls,
+    // pages: htmls,
     devServer: {
         proxy: {
             '/': {
@@ -47,7 +47,7 @@ module.exports = {
             }
         },
         open: true,
-        index: 'page1.html'
+        // index: 'page1.html'
     },
     chainWebpack: (config) => {
         let imgPath = './'
